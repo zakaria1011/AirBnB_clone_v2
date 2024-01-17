917251e9b1cdc9a26697709b6f0b922244870ac2
Ijbar Salah Eddine <ijbarsalaheddine@gmail.com>
2024-01-17 19:13:11 +0000

    SalahIj

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Show the grants for the user
+SHOW GRANTS FOR 'hbnb_dev'@'localhost';
