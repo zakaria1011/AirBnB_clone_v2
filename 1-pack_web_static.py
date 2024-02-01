e6b429d11f8a09410b4952a97d18f579fe85bbc8
zakaria <grarizakaria@gmail.com>
2024-02-01 16:10:17 +0100

    alx

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
 """ Archive the web static folder """
 
-from fabric.api import task, local
+from fabric.api import local
 from datetime import datetime
 import os
-
-WEB_STATIC_PATH = 'web_static'
-VERSIONS_PATH = 'versions'
 
 
 def do_pack():
     """ """
-    if not os.path.exists(VERSIONS_PATH):
-        os.makedirs(VERSIONS_PATH)
+    if not os.path.exists('versions'):
+        local('mkdir -p versions')
 
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = 'web_static_{}'.format(timestamp)
-    archive_path = os.path.join(VERSIONS_PATH, archive_name)
+    file_path = 'versions/web_static_{}.tgz'.format(timestamp)
 
-    tar_command = 'tar -cvzf {} {}'.format(archive_path, WEB_STATIC_PATH)
+    local('tar -cvzf {}'.format(file_path))
 
-    local(tar_command)
-    size = os.path.getsize(archive_path)
+    size = os.path.getsize(file_path)
     if os.path.exists(archive_path):
-        print('web_static packed: {} -> {} Bytes'.format(archive_path, size))
-        return archive_path
+        print('web_static packed: {} -> {} Bytes'.format(file_path, size))
+        return file_path
     else:
         print('Failed to create the archive.')
         return None
