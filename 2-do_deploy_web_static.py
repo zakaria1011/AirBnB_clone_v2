b6b7b36ae30b754889918c96f7d9862929e65432
zakaria <grarizakaria@gmail.com>
2024-02-04 15:50:48 +0100

    alx

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,18 +24,18 @@ def do_deploy(archive_path):
     run(
         'tar -xzf /tmp/{}'
         '-C /data/web_static/releases/{}/'.format(file_name, folder_name)
-        )
+    )
     run('rm /tmp/{}'.format(file_name))
     run(
         'mv /data/web_static/releases/{}/web_static/*'
         '/data/web_static/releases/{}/'.format(folder_name, folder_name)
-        )
+    )
     run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
     run('rm -rf /data/web_static/current')
     run(
         'ln -s /data/web_static/releases/{}/'
         '/data/web_static/current'.format(folder_name)
-        )
+    )
     print('New version deployed!')
     return True
 
