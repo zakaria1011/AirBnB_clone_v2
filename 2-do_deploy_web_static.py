fb7bc386dde7b47e75fddb9416b5a47b4253b536
zakaria <grarizakaria@gmail.com>
2024-02-04 16:14:01 +0100

    alx

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ Fabric script that distributes an archive to web servers
 from fabric.api import env, put, run, local
 from os.path import exists
 
-env.hosts = ['107.23.119.223', '54.89.118.36']  # Replace with your actual IP addresses
+env.hosts = ['107.23.119.223', '54.89.118.36']
 env.user = 'ubuntu'  # Replace with your SSH username
 
 
@@ -17,20 +17,20 @@ def do_deploy(archive_path):
     if not exists(archive_path):
         return False
 
-    # Upload the archive to the /tmp/ directory of the web server
     put(archive_path, '/tmp/')
 
-    # Extract archive to the folder /data/web_static/releases/<archive filename without extension>
     file_name = archive_path.split('/')[-1]
     folder_name = file_name.split('.')[0]
     run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, folder_name))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        .format(file_name, folder_name))
 
     # Delete the archive from the web server
     run('rm /tmp/{}'.format(file_name))
 
     # Move contents of web_static folder to the release folder
-    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
+    run('mv /data/web_static/releases/{}/web_static/* '
+        '/data/web_static/releases/{}/'.format(folder_name, folder_name))
 
     # Remove unnecessary web_static folder
     run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
@@ -39,7 +39,8 @@ def do_deploy(archive_path):
     run('rm -rf /data/web_static/current')
 
     # Create a new symbolic link /data/web_static/current
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        .format(folder_name))
 
     print('New version deployed!')
 
