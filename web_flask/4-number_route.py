59140ac1963847bcfefeacdf9a4564d45a3b7a2f
zakaria <grarizakaria@gmail.com>
2024-02-16 09:57:24 +0100

    alx

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
+""" flask web application """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ retunr hello HBNB """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ return hbnb """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """ return text """
     text = text.replace('_', ' ')
     return "C {}".format(text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
+    """ return number """
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
+    """ return number """
     return "{} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
