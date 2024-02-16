4a45911f02787cd3f088ed8dc0a12983466bfe3f
zakaria <grarizakaria@gmail.com>
2024-02-16 09:54:42 +0100

    alx

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
+""" flask web application """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ return hello HBNB """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ return hbnb """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """ return text """
     text = text.replace('_', ' ')
     return "C {}".format(text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
+    """ return variable number """
     text = text.replace('_', ' ')
     return "Python {}".format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
